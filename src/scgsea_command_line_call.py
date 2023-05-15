eb142e9a06c3871a7ba2adacae601d138caa06c0
John Jun <111310290+JohnSpJun@users.noreply.github.com>
2023-05-15 14:04:00 -0700

    Update scgsea_command_line_call.py

--- src/scgsea_command_line_call.py
+++ src/scgsea_command_line_call.py
@@ -9,12 +9,10 @@ ROOT = os.path.join(WORKING_DIR, '..')
 TASKLIB = os.path.join(ROOT, 'src/')
 INPUT_FILE_DIRECTORIES = os.path.join(ROOT, 'data/')
 
-command_line = "python "+TASKLIB+"ExampleModule.py"\
+command_line = "python "+TASKLIB+"scgsea.py"\
                 + " -f " + INPUT_FILE_DIRECTORIES+"data_placeholder.txt"\
-                + " -a True"\
-                + ' -m "This message has been added."'\
                 + " -o created_file_ground_truth"
-                + " -d"
+
 print("About to call the module using the command line:", command_line)
 
 call(command_line, shell=True)
