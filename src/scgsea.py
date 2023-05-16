186625f1feb25b56f5b753a2adb90e3739151389
John Jun <111310290+JohnSpJun@users.noreply.github.com>
2023-05-16 16:46:23 -0700

    Update scgsea.py

--- src/scgsea.py
+++ src/scgsea.py
@@ -19,6 +19,11 @@ parser.add_argument("--gene_set_database_file",
                     help="gene set",
                     default='False')
 
+parser.add_argument("--output_file_name",
+                    type=str,
+                    help="gene set",
+                    default='False')
+
 args = parser.parse_args()
 # if args.verbose:
 #     print("Ah! The old verbosaroo")
@@ -45,55 +50,20 @@ metacell_exp = pd.read_csv(args.input_file, index_col = 0)
 gs = pd.read_csv(args.gene_set_database_file, index_col = 0, header = None)
 
 print("loaded both metacell and gene set")
-# # Check for cluster annotation metadata
-# if 'cluster' not in data.obs.keys():
-#   raise ValueError("cluster annotation metadata not found.")
 
-# # Normalize the dataset using CLR across cells
-# norm_data = do_normalize(data, axis = 0, method = "clr")
+scGSEA_scores = single_sample_gseas(
+    metacell_exp,
+    gs,
+    plot=True,
+    title=None,
+    gene_score_name=None,
+    annotation_text_font_size=16,
+    annotation_text_width=88,
+    annotation_text_yshift=64,
+    html_file_path="{}_plot".format(args.output_file_name,
+    plotly_html_file_path=None)
 
-# # Subset the data by celltype
-# for ct in norm_data.obs['celltype'].unique():
-#   norm_data_by_celltype['{}'.format(ct)] = norm_data[norm_data.obs['cell_type'] == ct]
-  
-# # Aggregate the cells by metacell
-# exp_norm_by_celltype_aggregated = {}
-# for key in exp_norm_by_celltype.keys():
-#     exp_norm_by_celltype_aggregated[key] = do_aggregate(exp_norm_by_celltype[key], method = 'mean', aggregate_all = True)
+scGSEA_scores.to_csv(args.output_file_name)
 
-# # Perform ssGSEA
-# start_all = time.time()
-
-# for comb in keys:
-#     ct = comb[:comb.find('_')].replace(" ", "_")
-#     comb_w_underscore = comb.replace(" ", "_")
-
-# #### ---------- GENERATE PERMUTATIONS ---------- ####
-# #     rank = "counts" in comb
-# #     print("Generating Permutation for {} {}".format(comb, "-- Rank Normalize" if rank else ""))
-# #     perm_cell = permute_ties_parallel(norm_exp_aggregated[comb], 50, rank, n_job = 8)
-# #     perm_cell = [s for subset in perm_cell for s in subset]
-
-    
-# # Compute the score using KS
-# temp = run_ssgsea_parallel_with_permutation_permlist(
-#   perm_cell, gmt, n_perm = 50, stat = 'ks', n_job = 32)
-#   temp.to_csv("../results/{}_{}_50perms_computational_genesets_20expgenes.csv".format(comb_w_underscore.replace("/", "_"), metric), sep='\t', header=True, index=True)
-        
-#         print("--------------------------------------------------------------------------------")        
-#         print("\t Completed ssGSEA for {} using {} - 50 permutations".format(comb, metric))
-        
-# end_all = time.time()
-
-# print("--------------------------------------------------------------------------------")        
-# print("\t Total Time spent: {} ".format(end_all - start_all))
-
-
-# with open(out_filename, 'w') as g:
-#   for line in f.readlines():
-#     g.write(line)
-#     g.write('\n')  # This could be an argument, wether or not to add a newline.
-# f.close()
-
-# end_of_time = timer()
-# print("We are done! Wall time elapsed:", humanfriendly.format_timespan(end_of_time - beginning_of_time))
+end_of_time = timer()
+print("We are done! Wall time elapsed:", humanfriendly.format_timespan(end_of_time - beginning_of_time))
