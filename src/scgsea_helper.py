94e32bff7b6baad376f9b004b29dd4e434adff3a
John Jun <111310290+JohnSpJun@users.noreply.github.com>
2023-05-16 23:38:19 -0700

    Update scgsea_helper.py

--- src/scgsea_helper.py
+++ src/scgsea_helper.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 #NB - all of these import statements should specify their versions and be executed in a separate script at Docker build time.
+
+import pandas as pd
+
 # Here is where I'd put my functions, if I had any!
 def single_sample_gsea(
     gene_score,
