3317c9515eb95a9b8c8aeda189e39038f4b98147
John Jun <111310290+JohnSpJun@users.noreply.github.com>
2023-05-19 10:44:47 -0700

    Create scgsea_wrapper.py

---
+++ src/scgsea_wrapper.py
@@ -0,0 +1,35 @@
+import subprocess
+import argparse
+import os
+
+if os.path.exists('normalize_and_aggregate.R'):
+  print("found it")
+if os.path.exists('scgsea.py'):
+  print("found it")
+
+
+# parser = argparse.ArgumentParser()
+# # ~~~~Module Required Arguments~~~~~ #
+# parser.add_argument("--input_file",
+#                     type=str,
+#                     help="Input file",
+#                     default='False')
+
+# parser.add_argument("--gene_set_database_file",
+#                     type=str,
+#                     help="gene set",
+#                     default='False')
+
+# parser.add_argument("--output_file_name",
+#                     type=str,
+#                     help="scGSEA scores",
+#                     default='False')
+
+# parser.add_argument("--chip_file",
+#                     type=str,
+#                     help="chip file",
+#                     default='False')
+
+# args = parser.parse_args()
+
+# subprocess.run(['Rscript', ]
