05545cdf049aa6320b0566110696765783011473
John Jun <111310290+JohnSpJun@users.noreply.github.com>
2023-05-19 19:36:54 -0700

    Update run_scgsea.py

--- src/run_scgsea.py
+++ src/run_scgsea.py
@@ -52,10 +52,10 @@ print("Loaded CHIP file!\n")
 # Load the gene set database files
 print(f"Loading {args.gene_set_database_file} to convert to Gene Symbol")
 print('==========================================================')
-if args.gene_set_database_file.endswith(".txt"):
-  gs = read_gmts(args.gene_set_database_file)
-elif args.gene_set_database_file.endswith(".gmt"):
-  gs = read_gmt(args.gene_set_database_file)
+# if args.gene_set_database_file.endswith(".txt"):
+gs = read_gmts(args.gene_set_database_file)
+# elif args.gene_set_database_file.endswith(".gmt"):
+#   gs = read_gmt(args.gene_set_database_file)
 gs.to_csv("geneset_loaded.csv") # Test
 print("Loaded gene set file!\n")
 
